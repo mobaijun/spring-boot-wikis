6007d36783b9bcd737363d01d73f1c69e7f0d76a
mobaijun <mobaijun8@163.com>
2021-07-05 15:00:35 +0800

    first commit

---
+++ spring-boot-hello/src/test/java/com/mobai/SpringBootHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
