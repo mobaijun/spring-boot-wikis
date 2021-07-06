46f148bf574ed96d1ffd88e73087035f29a1c357
mobaijun <mobaijun8@163.com>
2021-07-06 16:13:42 +0800

    spring-boot-profile

--- spring-boot-profile/src/main/java/com/mobai/config/WxMpParam.java
+++ spring-boot-profile/src/main/java/com/mobai/config/WxMpParam.java
@@ -19,27 +19,27 @@ import org.springframework.stereotype.Component;
 // 启用配置自动注入功能
 @EnableConfigurationProperties
 // 指定类对应的配置项前缀
-// @ConfigurationProperties(prefix = "wxmp")
+@ConfigurationProperties(prefix = "wxmp")
 // 指定配置文件及编码
-@PropertySource(value = "classpath:application-wxmp.yml", encoding = "utf-8")
+// @PropertySource(value = "classpath:application-wxmp.yml", encoding = "utf-8")
 public class WxMpParam {
 
     // 注入wxmp.appid配置项
-    @Value("${wxmp.appid}")
-    private String appid;
+    // @Value("${wxmp.appid}")
+    // private String appid;
 
     // 注入wxmp.secret配置项
-    @Value("${wxmp.secret}")
-    private String secret;
+    // @Value("${wxmp.secret}")
+    // private String secret;
 
     /**
      * 如果参数很多，一一指定对象属性和配置项的关联非常麻烦。
      * 可以通过设定对象与配置项的对应关系，来实现配置项的自动注入。
      */
     // 对应到wxmp.appid
-    // private String appid;
+    private String appid;
     // 对应到wxmp.secret
-    // private String secret;
+    private String secret;
     /**
      * 在上面的代码中，通过 prefix = "wxmp" 指定了关联配置的前缀，属性 appid 即关联到前缀 + 属性名为 wxmp.appid 的配置项。
      * 同理，属性 secret 关联到 wxmp.secret 配置项。
