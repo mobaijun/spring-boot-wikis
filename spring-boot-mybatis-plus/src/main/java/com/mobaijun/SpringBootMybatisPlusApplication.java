5512c651d248fc7ded9b7f9884a4557abab86880
mobaijun <mobaijun8@163.com>
2021-07-20 14:24:26 +0800

    spring-boot-mybatis-plus

---
+++ spring-boot-mybatis-plus/src/main/java/com/mobaijun/SpringBootMybatisPlusApplication.java
@@ -0,0 +1,16 @@
+package com.mobaijun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMybatisPlusApplication {
+
+    /**
+     * 启动方法
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMybatisPlusApplication.class, args);
+    }
+
+}
