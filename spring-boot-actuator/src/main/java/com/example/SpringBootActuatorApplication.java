d9e2d5ba0c8d8a1c1ca5bf3331e4c8a3a6df7d79
mobaijun <mobaijun8@163.com>
2021-08-05 15:11:59 +0800

    spring-boot-actuator

---
+++ spring-boot-actuator/src/main/java/com/example/SpringBootActuatorApplication.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootActuatorApplication {
+
+    /**
+     * 请求路径：https://www.mobaijun.com/posts/283491354.html
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootActuatorApplication.class, args);
+    }
+
+}
