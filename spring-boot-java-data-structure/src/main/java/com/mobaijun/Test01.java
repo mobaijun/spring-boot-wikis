db0e3751f1f86b9d67e9253a638dd03d1a514e53
mobaijun <mobaijun8@163.com>
2022-02-12 22:30:09 +0800

    spring-boot-java-data-structure

---
+++ spring-boot-java-data-structure/src/main/java/com/mobaijun/Test01.java
@@ -0,0 +1,12 @@
+package com.mobaijun;
+
+/**
+ * Software：IntelliJ IDEA 2021.2 x64
+ * Author: https://www.mobaijun.com
+ * Date: 2022/2/12 20:53
+ * ClassName:Test01
+ * 类描述： 几个经典的算法面试题
+ * 视频链接：https://www.bilibili.com/video/BV1E4411H73v?from=search&seid=11175594417814011666&spm_id_from=333.337.0.0
+ */
+public class Test01 {
+}
