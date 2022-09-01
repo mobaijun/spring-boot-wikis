904c84fe7726c2f0f2e50a2fe8d5178be4728113
mobaijun <mobaijun8@163.com>
2022-09-01 09:50:07 +0800

    :ambulance: 修改不规范

--- spring-boot-mysql-redis/src/main/java/com/mobaijun/mapper/SystemUserMapper.java
+++ spring-boot-mysql-redis/src/main/java/com/mobaijun/mapper/SystemUserMapper.java
@@ -5,7 +5,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
 /**
  * Software：IntelliJ IDEA 2021.2 x64
- * Author: https://www.mobaijun.com
+ * Author: <a href="https://www.mobaijun.com">...</a>
  * Date: 2021/10/21 17:11
  * ClassName:User
  * 类描述： mapper接口
