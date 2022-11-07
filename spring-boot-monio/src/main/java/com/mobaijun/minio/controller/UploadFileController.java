27b4b905ee0f997a177fa72f683f7d20768c9b3d
mobaijun <mobaijun8@163.com>
2022-11-07 15:54:20 +0800

    :ambulance: 测试 minio

--- spring-boot-monio/src/main/java/com/mobaijun/minio/controller/UploadFileController.java
+++ spring-boot-monio/src/main/java/com/mobaijun/minio/controller/UploadFileController.java
@@ -3,6 +3,7 @@ package com.mobaijun.minio.controller;
 import com.mobaijun.minio.exception.MinioException;
 import com.mobaijun.minio.util.MinioService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +12,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -31,7 +34,18 @@ public class UploadFileController {
     private final MinioService minioService;
 
     @PostMapping("/upload")
-    public void upload(@RequestParam(name = "multipartFile") MultipartFile multipartFile) throws IOException, MinioException {
-        minioService.upload(Paths.get(Objects.requireNonNull(multipartFile.getOriginalFilename())), multipartFile.getInputStream(), "image/jpeg");
+    public String upload(@RequestParam(name = "multipartFile") MultipartFile multipartFile) throws IOException, MinioException {
+        return minioService.upload(multipartFile.getInputStream(), Paths.get(Objects.requireNonNull(multipartFile.getOriginalFilename())), "image/jpeg");
+    }
+
+    @GetMapping("/selectString")
+    public String selectString() throws MinioException {
+        return minioService.getUrl("o.jpg");
+    }
+
+    @GetMapping("/selectBucket")
+    public List<String> selectBucket() throws Exception {
+        minioService.removeObject("test-mb",minioService.listObjectNames("test-mb"));
+        return new LinkedList<>();
     }
 }
